6a6653815a4a365e460de91b34af1d6ad0a1c706
Yoshiki Kishimoto <0126yoshiki@icloud.com>
2018-04-14 11:47:35 +0900

    音楽が鳴らず苦戦したが、関連付けがしっかりできていなかったためとわかり作れた。

--- Music/ViewController.swift
+++ Music/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     var audioPlayer: AVAudioPlayer!
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
